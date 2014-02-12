63594b2648b48cb65a6f7715d01c925bb1e1fe11
sunyan1988 <sunyan1988@users.noreply.github.com>
2014-02-12 15:22:20 +0800

    Update AppDelegate.m
    delete no-need words

--- ControllingSource/AppDelegate.m
+++ ControllingSource/AppDelegate.m
@@ -37,10 +37,7 @@
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
+
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
